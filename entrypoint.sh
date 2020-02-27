c9048a941eb4e340f8d98603c7d8f351a005b5c8
kizzii <apisit.kizzii@gmail.com>
2020-02-27 12:50:09 +0700

    Re-strucutre files and directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 error() {
-  echo "You need to define DEFAULT_BOT_TOKEN, DEFAULT_CHAT_ID or Create 'config.yaml'" >&2;
+  echo "You need to spicify either DEFAULT_BOT_TOKEN, DEFAULT_CHAT_ID or Create 'config.yaml'" >&2
+  echo "See https://hub.docker.com/r/kizzii/telegram-alertmanager" >&2;
   exit 1;
 }
 
